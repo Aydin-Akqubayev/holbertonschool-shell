5724ea40e11cb3c31ad6bef34503e29c9e808953
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-16 18:37:29 +0400

    Create a new sentence

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+a=str.split(" ")
+str = a[5]+" "+ a[6]+" "+a[12]+" "+a[0]
+print(str)
