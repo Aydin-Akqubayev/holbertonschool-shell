8c57b5a02084db03136c0c32812d29d949ea0625
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-17 19:31:34 +0400

    new

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,10 @@ import sys
 def main():
     list_command = sys.argv
     length = len(list_command)
-    print("{} arguments:".format(length - 1))
+    if length - 1 > 1:
+        print("{} arguments:".format(length - 1))
+    else:
+        print("{} argument:".format(length - 1))
     for i in range(1, length):
         print("{}: {}".format(i, list_command[i]))
 
