8b6db16ae23e2bfc3f782302bd1ddf89a241e173
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-21 20:04:01 +0400

    update

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(text):
     length_text = len(text)
-    first_text = text[0]
+    if length_text == 0:
+        first_text = None
+    else:
+        first_text = text[0]
     return length_text, first_text
